38b0e462f738453e68597af94a5813e9c7e747b3
cipriann12 <cipriangombar13@gmail.com>
2024-04-27 13:33:19 +0200

    complete iteration 6

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -146,7 +146,10 @@ const wordsFind = [
   "disobedience",
 ];
 
-function doesWordExist() {}
+function doesWordExist(array, word) {
+  if (!array.length) return null;
+  return array.includes(word);
+}
 
 // Iteration #7: Count repetition
 const wordsCount = [
